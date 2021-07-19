024fdc75a80a830d28c274deeb952c4abff586d1
shreyanshyad <shreyansh.yadav.cse19@itbhu.ac.in>
2021-07-19 14:02:15 +0530

    squash 3

--- node-test/src/test/scala/org/bitcoins/node/NeutrinoNodeTest.scala
+++ node-test/src/test/scala/org/bitcoins/node/NeutrinoNodeTest.scala
@@ -2,7 +2,6 @@ package org.bitcoins.node
 
 import akka.actor.Cancellable
 import org.bitcoins.crypto.DoubleSha256DigestBE
-import org.bitcoins.rpc.client.v21.BitcoindV21RpcClient
 import org.bitcoins.server.BitcoinSAppConfig
 import org.bitcoins.testkit.BitcoinSTestAppConfig
 import org.bitcoins.testkit.node.fixture.NeutrinoNodeConnectedWithBitcoinds
